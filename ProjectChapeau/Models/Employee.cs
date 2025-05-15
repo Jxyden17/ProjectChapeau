b74677117849a4982cc9e3e0f06746421158783b
Jxyden_ <ejsmeulemans@cs.com>
2025-05-15 14:26:20 +0200

    Commit to personal dev branch

--- ProjectChapeau/Models/Employee.cs
+++ ProjectChapeau/Models/Employee.cs
@@ -2,5 +2,24 @@
 {
     public class Employee
     {
+
+        public int employeeId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public bool isActive { get; set; }
+        public string role { get; set; }
+
+        public Employee(int employeeId, string firstName, string lastName, string userName, string password, bool isActive, string role)
+        {
+            this.employeeId = employeeId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.userName = userName;
+            this.password = password;
+            this.isActive = isActive;
+            this.role = role;
+        }
     }
 }
---
+++ ProjectChapeau/Models/RestaurantTable.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectChapeau.Models
+{
+    public class RestaurantTable
+    {
+        public int tableId {  get; set; }
+        public bool seatingStatus { get; set; }
+
+        public RestaurantTable(int tableId, bool seatingStatus)
+        {
+            this.tableId = tableId;
+            this.seatingStatus = seatingStatus;
+        }
+    }
+}
