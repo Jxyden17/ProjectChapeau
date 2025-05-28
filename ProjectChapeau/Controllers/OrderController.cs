937e37accf091358c0795fa85115a8ca9177bd5d
Aloys Stekkinger <153507008+Sanikku5@users.noreply.github.com>
2025-05-28 12:11:13 +0200

    OrderController is merged into one file, no redundant files anymore

--- ProjectChapeau/Controllers/OrderController.cs
+++ ProjectChapeau/Controllers/OrderController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectChapeau.Models;
+using ProjectChapeau.Services.Interfaces;
+using ProjectChapeau.Views.ViewModel;
 
 namespace ProjectChapeau.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IMenuItemService _menuItemsService;
         public IActionResult Index()
         {
-            return View();
+            // Placeholder redirect
+            return RedirectToAction("Menu", "Orders");
+        }
+        public OrderController(IMenuItemService menuItemsService)
+        {
+            _menuItemsService = menuItemsService;
+        }
+        public IActionResult Menu()
+        {
+            MenusOverviewViewModel menusOverviewViewModel = new(_menuItemsService.GetAllMenuItems());
+            return View(menusOverviewViewModel);
+        }
+        public IActionResult MenuItem(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound("There is no menu item ID provided in the URL. Please go back and try again.");
+            }
+            MenuItem? menuItem = _menuItemsService.GetMenuItemById((int)id);
+
+            if (menuItem == null)
+            {
+                return NotFound($"Menu item with ID {id} does not exist");
+            }
+            return View(menuItem);
         }
     }
 }
