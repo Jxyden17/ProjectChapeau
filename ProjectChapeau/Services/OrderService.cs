ee9a63b1f5eb289e7796b99726f55485a5ae7875
Jxyden_ <ejsmeulemans@cs.com>
2025-05-28 13:48:27 +0200

    Added orderitems

--- ProjectChapeau/Services/OrderService.cs
+++ ProjectChapeau/Services/OrderService.cs
@@ -8,15 +8,23 @@ namespace ProjectChapeau.Services
     {
 
         private readonly IOrderRepository _orderRepository;
+        private readonly IOrderItemRepository _orderItemRepository;
 
-        public OrderService(IOrderRepository orderRepository)
+        public OrderService(IOrderRepository orderRepository, IOrderItemRepository orderItemRepository)
         {
             _orderRepository = orderRepository;
+            _orderItemRepository = orderItemRepository;
         }
 
         public List<Order> GetAllOrders()
         {
-           return _orderRepository.GetAllOrders();
+            List<Order> orders = _orderRepository.GetAllOrders();
+
+            foreach (Order order in orders)
+            {
+                order.orderItems = _orderItemRepository.GetAllOrderItemsById(order.orderId);
+            }
+            return orders;
         }
         public List<Order> GetRunningOrders()
         {
