16cfdb157777efe0c323751054332c81a6b6a42b
Aloys Stekkinger <153507008+Sanikku5@users.noreply.github.com>
2025-06-30 05:32:06 +0200

    improve styling

--- ProjectChapeau/Controllers/OrderController.cs
+++ ProjectChapeau/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcWhatsUp.Models.Extensions;
 using ProjectChapeau.Models;
+using ProjectChapeau.Models.Enums;
 using ProjectChapeau.Models.ViewModel;
 using ProjectChapeau.Services;
 using ProjectChapeau.Services.Interfaces;
@@ -8,25 +10,26 @@ namespace ProjectChapeau.Controllers
 {
     public class OrderController : Controller
     {
+        private const string orderSessionKey = "OrderSession";
 
         private readonly IMenuService _menuService;
         private readonly IMenuItemService _menuItemService;
         private readonly IOrderService _orderService;
-        
+
         public OrderController(IMenuService menuService, IMenuItemService menuItemService, IOrderService orderService)
         {
             _menuService = menuService;
             _menuItemService = menuItemService;
             _orderService = orderService;
         }
-       
-            public IActionResult Index()
+
+        public IActionResult Index()
         {
             List<Order> runningOrders = _orderService.GetRunningOrders();
             return View(runningOrders);
 
         }
-       
+
         public IActionResult Menu()
         {
             MenusOverviewViewModel menusOverviewViewModel = new(_menuService.GetAllMenus());
@@ -47,6 +50,36 @@ namespace ProjectChapeau.Controllers
             MenuItemOverviewViewModel menuItemOverviewViewModel = new(menuItem);
 
             return View(menuItemOverviewViewModel);
+        }
+        public IActionResult StartOrder()
+        {
+            Order order = new();
+            HttpContext.Session.SetObject(orderSessionKey, order);
+
+            return View();
+        }
+        public IActionResult AddItemToOrder(int? menuItemId, int? amount, string? comment)
+        {
+            try
+            {
+                Order? order = HttpContext.Session.GetObject<Order>(orderSessionKey);
+
+                if (menuItemId == null)
+                {
+                    throw new ArgumentNullException(nameof(menuItemId));
+                }
+                int quantity = amount ?? 1;
+                MenuItem menuItem = _menuItemService.GetMenuItemById(menuItemId.Value);
+                OrderLine orderLine = new(menuItem, quantity, comment, OrderStatus.NotOrdered);
+                order.OrderLines.Add(orderLine);
+                HttpContext.Session.SetObject<Order>(orderSessionKey, order);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = $"Item could not be added to current order: {ex.Message}";
+                return View();
+            }
         }
     }
 }
